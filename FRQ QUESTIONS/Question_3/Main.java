ef677676c43707b433c7abfc4d9a49208f9b1c17
arzoa1 <arzoa@nycstudents.net>
2024-05-29 23:33:19 -0400

    Update Main.java

--- FRQ QUESTIONS/Question_3/Main.java
+++ FRQ QUESTIONS/Question_3/Main.java
@@ -1,1 +1,28 @@
+import java.util.*;
+public class WordChecker{
+  private ArrayList<String> wordList;
 
+  public WordChecker(ArrayList<String> w){
+    wordList = w;
+  }
+
+  public boolean isWordChain(){
+    for(int i = 1; i < wordList.size(); i++){
+      if(wordList.get(i).indexOf(wordList.get(i-1)) == -1){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<String> createList(String target){
+    ArrayList<String> news = new ArrayList<String>();
+    for(int i = 0; i < wordList.size(); i++){
+      if(wordList.get(i).substring(0, target.length()).indexOf(target) == 0){
+        wordList.add(wordList.get(i).substring(target.length()));
+      }
+    }
+    return news;
+  }
+  
+}
