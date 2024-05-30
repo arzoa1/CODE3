7f7404ab91153633adf3e28adfc0952339d8a4fd
arzoa1 <arzoa@nycstudents.net>
2024-05-29 23:33:02 -0400

    Update WordChecker.java

--- FRQ QUESTIONS/Question_3/WordChecker.java
+++ FRQ QUESTIONS/Question_3/WordChecker.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class WordChecker{
+  private ArrayList<String> wordList;
 
+  public WordChecker(ArrayList<String> words){
+    wordList= words;
+  }
+
+  public boolean isWordChain(){
+    for(int )
+    return false;
+  }
+
+  public ArrayList<String> createList(String target){
+     ArrayList<String> result = new ArrayList<String> ();
+}
+
+  
+}
